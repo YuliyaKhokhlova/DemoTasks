9564f2664338eebd558c892875629ced67540b0f
Yuliya Kuzmina <girl.from.ff.nsu@gmail.com>
2015-10-26 22:19:10 +0600

    implemented depth- and breadth-first tree search

---
+++ TreeSearch-Tests/TreeSearchTests.cs
@@ -0,0 +1,60 @@
+﻿using TreeSearch;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TreeSearch_Tests
+{
+    [TestClass]
+    public class TreeSearchTests
+    {
+        [TestMethod]
+        public void DepthSearch6Nodes()
+        {
+            string expectedOutput = "1 2 5 6 3 4 ";
+            string output = Program.DepthSearch(tree6Nodes);
+            Assert.AreEqual(expectedOutput, output, "Unexpected order of tree searh");
+        }
+
+        [TestMethod]
+        public void DepthSearch12Nodes()
+        {
+            string expectedOutput = "1 2 3 4 5 6 7 8 9 10 11 12 ";
+            string output = Program.DepthSearch(tree12Nodes);
+            Assert.AreEqual(expectedOutput, output, "Unexpected order of tree searh");
+        }
+
+        [TestMethod]
+        public void BreadthSearch6Nodes()
+        {
+            string expectedOutput = "1 2 3 4 5 6 ";
+            string output = Program.BreadthSearch(tree6Nodes);
+            Assert.AreEqual(expectedOutput, output, "Unexpected order of tree searh");
+        }
+
+        [TestMethod]
+        public void BreadthSearch12Nodes()
+        {
+            string expectedOutput = "1 2 7 10 3 4 8 11 12 5 6 9 ";
+            string output = Program.BreadthSearch(tree12Nodes);
+            Assert.AreEqual(expectedOutput, output, "Unexpected order of tree searh");
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Tree with 6 nodes
+            // Nodes are enumerated according to breadth search order
+            tree6Nodes = new TreeNode(1);
+            tree6Nodes.AddChildren(new TreeNode(2, new TreeNode(5), new TreeNode(6)), new TreeNode(3), new TreeNode(4));
+
+            // Tree with 12 nodes
+            // Nodes are enumerated according to depth search order
+            tree12Nodes = new TreeNode(1);
+            TreeNode auxNode1 = new TreeNode(2, new TreeNode(3), new TreeNode(4, new TreeNode(5), new TreeNode(6)));
+            TreeNode auxNode2 = new TreeNode(7, new TreeNode(8, new TreeNode(9)));
+            TreeNode auxNode3 = new TreeNode(10, new TreeNode(11), new TreeNode(12));
+            tree12Nodes.AddChildren(auxNode1, auxNode2, auxNode3);
+        }
+        public TreeNode tree6Nodes;
+        public TreeNode tree12Nodes;
+    }
+}
---
+++ TreeSearch/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TreeSearch
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            TreeNode tree = new TreeNode(1);
+            TreeNode node = new TreeNode(2);
+            tree.AddChildren(node, new TreeNode(3), new TreeNode(4));
+            node.AddChildren(new TreeNode(5), new TreeNode(6));
+
+            Console.WriteLine("Breadth Search:");
+            BreadthSearch(tree);
+
+            Console.WriteLine();
+            Console.WriteLine("Depth Search:");
+            DepthSearch(tree);
+        }
+
+        public static string DepthSearch(TreeNode tree)
+        {
+            // Build the stack of the tree search
+            Stack<Record> stack = new Stack<Record>();
+
+            // Start from the root
+            stack.Push(new Record(tree, 0));
+
+            string output = "";
+            while (stack.Count > 0)
+            {
+                Record topRecord = stack.Peek();
+                TreeNode current = topRecord.Node;
+                int childrenChecked = topRecord.ChildrenChecked;
+
+            // Process the node when the first time discovered
+                if (0 == childrenChecked)
+                {
+                    ProcessNode(current, ref output);
+                }
+
+            // Remove the node from stack when no more children to process
+                if (childrenChecked == current.Children.Count)
+                {
+                    stack.Pop();
+                    continue;
+                }
+            
+            // Put the next not processed child on the stack
+                topRecord.ChildrenChecked += 1;
+                stack.Push(new Record( current.Children[childrenChecked], 0 ));
+            }
+
+            return output;
+        }
+
+        public static string BreadthSearch(TreeNode tree)
+        {
+            // Build the queue of the tree search
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            
+            // Start from the root
+            queue.Enqueue(tree);
+
+            string output = "";
+            while (queue.Count > 0)
+            {
+                TreeNode current = queue.Dequeue();
+                ProcessNode(current, ref output);
+
+            // Add all children of the current node to the queue
+                foreach(TreeNode node in current.Children)
+                {
+                    queue.Enqueue(node);
+                }
+            }
+
+            return output;
+        }
+
+        public static void ProcessNode(TreeNode node, ref string output)
+        {
+            Console.Write(node.Value + " ");
+            output += node.Value + " ";
+        }
+    }
+
+    public class Record
+    {
+        public TreeNode Node { get; set; }
+        public int ChildrenChecked { get; set; }
+
+        public Record(TreeNode node, int childrenChecked)
+        {
+            Node = node;
+            ChildrenChecked = childrenChecked;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int Value { get; set; }
+
+        private List<TreeNode> _children = new List<TreeNode>();
+        public List<TreeNode> Children {
+            get {
+                return _children;
+            }
+        }
+
+        public TreeNode(int val)
+        {
+            Value = val;
+        }
+
+        public TreeNode(int val, params TreeNode[] children)
+        {
+            Value = val;
+            AddChildren(children);
+        }
+
+        public void AddChildren(params TreeNode[] children)
+        {
+            foreach (TreeNode node in children)
+            {
+                if (null != node)
+                {
+                    _children.Add(node);
+                }
+            }
+        }
+    }
+}
