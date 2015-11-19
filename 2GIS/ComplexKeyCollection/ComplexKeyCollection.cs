815484e22a222ae1d9b57ef4c2acd8557ff32e7b
Yuliya Kuzmina <girl.from.ff.nsu@gmail.com>
2015-11-19 16:45:26 +0600

    ComplexKeyCollection

---
+++ 2GIS/ComplexKeyCollection/ComplexKeyCollection.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexKeyCollection
+{
+    class ComplexKey<TId, TName>
+        where TId : IEqualityComparer<TId>
+        where TName : IEqualityComparer<TName>
+    {
+        public TId Id { get; }
+        public TName Name { get; }
+
+        public ComplexKey(TId idValue, TName nameValue)
+        {
+            Id = idValue;
+            Name = nameValue;
+        }
+
+        public bool Equals(ComplexKey<TId, TName> obj)
+        {
+            return Id.Equals(obj.Id) && Name.Equals(obj.Name);
+        }
+
+        public override bool Equals(Object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() + Name.GetHashCode();
+        }
+    }
+
+    class ComplexKeyCollection<TId, TName, TValue> : IDictionary<ComplexKey<TId, TName>, TValue> 
+        where TId : IEqualityComparer<TId>
+        where TName : IEqualityComparer<TName>
+    {
+        private Dictionary<ComplexKey<TId, TName>, TValue> collection;
+
+        public ComplexKeyCollection()
+        {
+            collection = new Dictionary<ComplexKey<TId, TName>, TValue>();
+        }
+
+        public bool ContainsKey(ComplexKey<TId, TName> key)
+        {
+            return ((IDictionary<ComplexKey<TId, TName>, TValue>)collection).ContainsKey(key);
+        }
+
+        public void Add(ComplexKey<TId, TName> key, TValue value)
+        {
+            ((IDictionary<ComplexKey<TId, TName>, TValue>)collection).Add(key, value);
+        }
+
+        public void Add(TId id, TName name, TValue value)
+        {
+            collection.Add(new ComplexKey<TId, TName>(id, name), value);
+        }
+
+        public bool Remove(ComplexKey<TId, TName> key)
+        {
+            return ((IDictionary<ComplexKey<TId, TName>, TValue>)collection).Remove(key);
+        }
+
+        public bool Remove(TId id, TName name)
+        {
+            return ((IDictionary<ComplexKey<TId, TName>, TValue>)collection).Remove(new ComplexKey<TId, TName>(id, name));
+        }
+
+        public bool TryGetValue(ComplexKey<TId, TName> key, out TValue value)
+        {
+            return ((IDictionary<ComplexKey<TId, TName>, TValue>)collection).TryGetValue(key, out value);
+        }
+
+        public void Add(KeyValuePair<ComplexKey<TId, TName>, TValue> item)
+        {
+            ((IDictionary<ComplexKey<TId, TName>, TValue>)collection).Add(item);
+        }
+
+        public void Clear()
+        {
+            ((IDictionary<ComplexKey<TId, TName>, TValue>)collection).Clear();
+        }
+
+        public bool Contains(KeyValuePair<ComplexKey<TId, TName>, TValue> item)
+        {
+            return ((IDictionary<ComplexKey<TId, TName>, TValue>)collection).Contains(item);
+        }
+
+        public void CopyTo(KeyValuePair<ComplexKey<TId, TName>, TValue>[] array, int arrayIndex)
+        {
+            ((IDictionary<ComplexKey<TId, TName>, TValue>)collection).CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(KeyValuePair<ComplexKey<TId, TName>, TValue> item)
+        {
+            return ((IDictionary<ComplexKey<TId, TName>, TValue>)collection).Remove(item);
+        }
+
+        public IEnumerator<KeyValuePair<ComplexKey<TId, TName>, TValue>> GetEnumerator()
+        {
+            return ((IDictionary<ComplexKey<TId, TName>, TValue>)collection).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IDictionary<ComplexKey<TId, TName>, TValue>)collection).GetEnumerator();
+        }
+
+        public ICollection<TValue> Values
+        {
+            get
+            {
+                return collection.Values;
+            }
+        }
+
+        public IEnumerable<TValue> ValuesWithName(TName name)
+        {
+            return collection.Where(e => e.Key.Name.Equals(name)).Select(e => e.Value);
+        }
+
+        public IEnumerable<TValue> ValuesWithId(TId id)
+        {
+            return collection.Where(e => e.Key.Id.Equals(id)).Select(e => e.Value);
+        }
+
+        public ICollection<ComplexKey<TId, TName>> Keys
+        {
+            get
+            {
+                return collection.Keys;
+            }
+        }
+
+        public IEnumerable<TId> IDs
+        {
+            get
+            {
+                return collection.Keys.Select(e => e.Id);
+            }
+        }
+
+        public IEnumerable<TName> Names
+        {
+            get
+            {
+                return collection.Keys.Select(e => e.Name);
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return ((IDictionary<ComplexKey<TId, TName>, TValue>)collection).Count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return ((IDictionary<ComplexKey<TId, TName>, TValue>)collection).IsReadOnly;
+            }
+        }
+
+        public TValue this[ComplexKey<TId, TName> key]
+        {
+            get
+            {
+                return ((IDictionary<ComplexKey<TId, TName>, TValue>)collection)[key];
+            }
+
+            set
+            {
+                ((IDictionary<ComplexKey<TId, TName>, TValue>)collection)[key] = value;
+            }
+        }
+
+        public TValue this[TId id, TName name]
+        {
+            get
+            {
+                return collection[new ComplexKey<TId, TName>(id, name)];
+            }
+            set
+            {
+                collection[new ComplexKey<TId, TName>(id, name)] = value;
+            }
+        }
+    }
+}
---
+++ 2GIS/ComplexKeyCollection/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexKeyCollection
+{
+    class NumberStr
+    {
+        public int Number { get; }
+        public string Str { get; }
+
+        public NumberStr(int n, string s)
+        {
+            Number = n;
+            Str = s;
+        }
+
+        public override string ToString()
+        {
+            return Number.ToString() + "|" + Str;
+        }
+    }
+
+    class Identifier : IEqualityComparer<Identifier>
+    {
+        public int MajorId { get; }
+        public int MinorId { get; }
+    
+        public Identifier(int major, int minor)
+        {
+            MajorId = major;
+            MinorId = minor;
+        }
+
+        public override string ToString()
+        {
+            return MajorId.ToString() + "|" + MinorId.ToString();
+        }
+
+        public bool Equals(Identifier obj)
+        {
+            return MajorId == obj.MajorId && MinorId == obj.MinorId;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public bool Equals(Identifier x, Identifier y)
+        {
+            return x.MajorId == y.MajorId && x.MinorId == y.MinorId;
+        }
+
+        int multiplier = 197;
+        public override int GetHashCode()
+        {
+            return multiplier * MajorId + MinorId;
+        }
+
+        public int GetHashCode(Identifier obj)
+        {
+            return multiplier * obj.MajorId + obj.MinorId;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ComplexKeyCollection<int, string, string> col = new ComplexKeyCollection<int, string, string>();
+            col[5, "hello"] = "Kitty";
+            col.Add(5, "go", "Molly");
+            col.Add(4, "hello", "Dolly");
+            ShowUsage<int, string, string>(col);
+
+            ComplexKeyCollection<Identifier, string, NumberStr> smth = new ComplexKeyCollection<Identifier, string, NumberStr>();
+            smth[new Identifier(5, 4), "milk"] = new NumberStr(15, "bowl");
+            smth.Add(new Identifier(3, 7), "juice", new NumberStr(2, "cup"));
+            smth.Add(new Identifier(3, 7), "water", new NumberStr(2, "glass"));
+            smth[new Identifier(5, 4), "milk"] = new NumberStr(15, "bottle");
+            ShowUsage<Identifier, string, NumberStr>(smth);
+            
+            Identifier a = new Identifier(5, 4);
+            Identifier b = new Identifier(5, 4);
+            if ("abc".GetHashCode().Equals("abc".GetHashCode()))
+            {
+                Console.WriteLine("abc".GetHashCode());
+                Console.WriteLine("Equal");
+            }
+            else
+                Console.WriteLine("Not Equal");
+        }
+
+        private static void ShowUsage<TId, TName, TValue>(ComplexKeyCollection<TId, TName, TValue> collection)
+            where TId : IEqualityComparer<TId>
+            where TName : IEqualityComparer<TName>
+        {
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Number of elements: " + collection.Count);
+            Console.WriteLine("ID: " + typeof(TId));
+            Console.WriteLine("Name: " + typeof(TName));
+            Console.WriteLine("Value: " + typeof(TValue));
+            foreach(KeyValuePair<ComplexKey<TId, TName>, TValue> element in collection)
+            {
+                Console.WriteLine("{0} - {1} - {2}", element.Key.Id, element.Key.Name, element.Value);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
